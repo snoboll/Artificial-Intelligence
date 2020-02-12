ee34024c8a18c688f02e01c0ca4e025cdf7b469a
Jesper Laurell <jesper.laurell1@gmail.com>
2020-02-12 10:38:38 +0100

    perceptron done

--- Assignment2/perceptron.py
+++ Assignment2/perceptron.py
@@ -1,6 +1,6 @@
 #perceptron
 import matplotlib.pyplot as plt
-import datasets
+import datasets, random
 import vector
 import numpy as np
 
@@ -18,17 +18,33 @@ w = [1, 1, 1]
 Xeye = [Xe[i] + [ye[i]] for i, x in enumerate(Xe)]#class 0
 Xfyf = [Xf[i] + [yf[i]] for i, x in enumerate(Xf)]#class 1
 allpoints = Xeye + Xfyf
-#eng = 0, fra = 1
-for _ in range(1000):
-    for i, x in enumerate(allpoints):
-        gold = 0 if i < 15 else 1
-        pred = 1 if np.dot(x, w) > 0 else 0
+score = 0
 
-        d = gold-pred
-        w[0] = w[0] + x[0]*d*rate
-        w[1] = w[1] + x[1]*d*rate
-        w[2] = w[2] + x[2]*d*rate
 
+for j in range(30):
+    allpoints_copy = allpoints.copy()
+    randint = random.randint(0,29)
+    p = allpoints_copy.pop(randint)
+    goldlist = [0]*15 + [1]*15
+    p_gold = goldlist.pop(randint)
+    #eng = 0, fra = 1
+    for _ in range(1000):
+        for i, x in enumerate(allpoints_copy):
+            pred = 1 if np.dot(x, w) > 0 else 0
+            gold = goldlist[i]
+
+            d = gold-pred
+            w[0] = w[0] + x[0]*d*rate
+            w[1] = w[1] + x[1]*d*rate
+            w[2] = w[2] + x[2]*d*rate
+
+    pred = 1 if np.dot(p, w) > 0 else 0
+
+    if (p_gold == pred):
+        score += 1
+print(score)
+
+plt.plot(p[1], p[2], 'go')
 x = np.linspace(10000, 80000, 1000)
 y = -(w[0] + w[1]*x)/w[2]
 plt.plot(x, y)
